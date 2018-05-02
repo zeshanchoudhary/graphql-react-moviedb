b2cf1899ce3d0c96808405dd3f4ac40deafa6e53
zeshanchoudhary <zeshan@doubleok.com>
2018-05-02 10:24:27 +0200

    App ejected

---
+++ client-react/zeshan/sum.spec.js
@@ -0,0 +1,21 @@
+const { sum } = require('./helper');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toEqual(3);
+});
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toEqual(3);
+});
+describe('Sum', () => {
+
+  it.only('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toEqual(3);
+  });
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toEqual(3);
+  });
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toEqual(3);
+  });
+})
